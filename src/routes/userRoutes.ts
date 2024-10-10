07d6b3b61d10772beeddd942d4b36c44d6b12d3e
Yonatan <yonatanforsh@gmail.com>
2024-10-10 16:57:09 +0300

    read and delete user done

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUser, getUser, getUsers } from "../controllers/userController";
+import { createUser, getUser, getUsers, deleteUser } from "../controllers/userController";
 
 const userRouter = Router();
 
 userRouter.post("/", createUser);
 userRouter.get("/", getUsers);
 userRouter.get("/:username", getUser);
+userRouter.delete("/:username", deleteUser);
+
 
 export default userRouter;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,28 +1,54 @@
 import UserModel, { IUser } from "../models/userModel" 
 import { userDTO } from "../dto/userDto"
 import { log } from "console"
+import mongoose from "mongoose"
+import userModel from "../models/userModel"
+import { Schema } from "inspector/promises"
 
 
 
 const createUser = async (user: IUser) => {
     try {
-        const newUser = new UserModel(user)
-        await newUser.save()
+        const newUser = new UserModel(user);
+        await newUser.save();
         return newUser
-    
-    } catch (err) {
-        throw new Error("invalid user")
+    } catch (err: any) {
+        if (err.code === 11000) {
+            throw new Error("Username already exists")
+        }
+        throw new Error("Invalid user");
+    }
+};
+
+
+
+const getUserById = async (username: string) => {
+    const user = await UserModel.findOne({username: username})
+    if(user) return user
+    else{
+        return "user not found"
     }
 }
 
 
-const getUserById = async (user: typeof UserModel) => {
-    
+const getUsers = async () => {
+    try {
+        const allUsers = await UserModel.find({})
+        return allUsers 
+    } catch (err) {
+        throw new Error("there aren't users yet")
+    }
 }
 
 
-const getUsers = async (user: typeof UserModel) => {
-
+const deleteUser = async (username: string) => {
+    const user = await UserModel.findOne({username: username})
+    if(user){     
+        await UserModel.deleteOne({username: username})
+        return user
+    } else {
+        return "user not found"
+    }
 }
 
 
@@ -36,5 +62,6 @@ export default {
     createUser,
     getUserById,
     getUsers,
+    deleteUser,
     updateUserDetails
 }
